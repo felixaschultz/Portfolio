6e5d37ecd5625b4e8cad9fb59dc4e68f01af069c
Felix A. Schultz <felix.schultz@engapho.com>
2023-12-28 19:30:07 +0100

    Add Chat Bot project

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -42,6 +42,24 @@ export const Projects = [
         type: "Web design / development"
     },
     {
+        id: "chat-bot",
+        name: "Chat Bot",
+        screenshot: null,
+        description: {
+            de: "",
+            da: "Under mit studie på Erhvervsakademiet Aarhus, fik jeg muligheden at udvikle en Chat Bot som et lille projekt. Jeg udviklede den i HTML & CSS og vanilla JS.",
+            en: ""
+        },
+        short_description: {
+            de: "",
+            da: "Under mit studie på Erhvervsakademiet Aarhus, fik jeg muligheden at udvikle en Chat Bot som et lille projekt. Jeg udviklede den i HTML & CSS og vanilla JS.",
+            en: ""
+        },
+        github: "https://github.com/felixaschultz/chatbot",
+        url: "https://github.com/felixaschultz/chatbot",
+        type: "Web design / development"
+    },
+    {
         id: "intastellarsolutions",
         name: "Intastellar Solutions, International",
         screenshot: intastellarsolutions,
@@ -75,24 +93,6 @@ export const Projects = [
         },
         github: null,
         url: "https://www.danfoss.com/en/products/dcs/monitoring-and-services/alsense-insite/",
-        type: "Web design / development"
-    },
-    {
-        id: "chat-bot",
-        name: "Chat Bot",
-        screenshot: null,
-        description: {
-            de: "",
-            da: "Under mit studie på Erhvervsakademiet Aarhus, fik jeg muligheden at udvikle en Chat Bot som et lille projekt. Jeg udviklede den i HTML & CSS og vanilla JS.",
-            en: ""
-        },
-        short_description: {
-            de: "",
-            da: "Under mit studie på Erhvervsakademiet Aarhus, fik jeg muligheden at udvikle en Chat Bot som et lille projekt. Jeg udviklede den i HTML & CSS og vanilla JS.",
-            en: ""
-        },
-        github: "https://github.com/felixaschultz/chatbot",
-        url: "https://github.com/felixaschultz/chatbot",
         type: "Web design / development"
     }
 ]

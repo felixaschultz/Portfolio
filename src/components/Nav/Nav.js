fecc91c449f09170e345bfa646f1e3d7cda9e4fe
Felix A. Schultz <felix.schultz@engapho.com>
2024-01-04 20:04:18 +0100

    Add language selector to navigation

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,7 +8,13 @@ import logo from "../../statics/assets/felix-schultz-logo.svg";
 
 export default function Nav() {
     const [location, setLocation] = useContext(LocationContext);
-    const locationPath = useLocation()
+    const locationPath = useLocation();
+    const [showLanguage, setShowLanguage] = useState(false);
+
+    const openLanguage = () => {
+        setShowLanguage(!showLanguage);
+    }
+
     return (
         <>
             {
@@ -22,15 +28,22 @@ export default function Nav() {
                         )
                     })
                 }
-                {
-                    local[location].navigation.langSelector.map((choosedLang, key) => {
-                        return <>
-                            <button key={choosedLang} className="locationPicker" onClick={(e) => {setLocation(choosedLang.short)}}>
-                                {choosedLang.long}
-                            </button>
-                        </>
-                    })
-                }
+                <section>
+                    <button onClick={openLanguage} className="locationPicker">{location}</button>
+                    {
+                        (showLanguage) ? <section className="language">
+                            {
+                                local[location].navigation.langSelector.map((choosedLang, key) => {
+                                    return <>
+                                        <button key={choosedLang} className="locationPicker" onClick={(e) => {setLocation(choosedLang.short), setShowLanguage(!showLanguage)}}>
+                                            {choosedLang.long}
+                                        </button>
+                                    </>
+                                })
+                            }
+                        </section> : null
+                    }
+                </section>
                 <a className="navigation__link" href="https://github.com/felixaschultz" target="_blank" role="button"><img className="github--icon" src="https://www.intastellarsolutions.com/assets/icons/GitHub-Mark/PNG/GitHub-Mark-64px.png" alt="Github" /></a>
                 <a className="navigation__link" href="https://www.linkedin.com/in/felixaschultz" target="_blank" role="button"> LinkedIn</a>
                 <iframe src="https://github.com/sponsors/felixaschultz/button" title="Sponsor felixaschultz" height="32" width="114" style={{border: 0, borderRadius: "6px"}}></iframe>

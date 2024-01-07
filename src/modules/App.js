d3afe96fe4b70ac4c6b46e6c3dc742d06757f16e
Felix A. Schultz <felix.schultz@engapho.com>
2024-01-07 01:37:29 +0100

    reseting word variable when word is matched

--- src/modules/App.js
+++ src/modules/App.js
@@ -26,26 +26,28 @@ export default function App() {
 
     let word = "";
 
-window.addEventListener("keydown", (e) => {
-    if (e.key === "Backspace") {
-        // Handle backspace separately
-        word = word.substring(0, word.length - 1);
-    } else if (e.key === "Shift") {
-        // Remove the entire "Shift" word and append uppercase letter
-        word += e.key.replace(/Shift/g, "").toUpperCase();
-    } else if (e.key.length === 1) {
-        // Handle regular alphanumeric keys
-        word += e.key;
-    }
+    window.addEventListener("keydown", (e) => {
+        if (e.key === "Backspace") {
+            // Handle backspace separately
+            word = word.substring(0, word.length - 1);
+        } else if (e.key === "Shift") {
+            // Remove the entire "Shift" word and append uppercase letter
+            word += e.key.replace(/Shift/g, "").toUpperCase();
+        } else if (e.key.length === 1) {
+            // Handle regular alphanumeric keys
+            word += e.key;
+        }
 
-    if (word === "dark") {
-        alert("Dark");
-    }
+        if (word === "dark") {
+            alert("Dark");
+            word = "";
+        }
 
-    if (word === "light") {
-        alert("Light");
-    }
-});
+        if (word === "light") {
+            alert("Light");
+            word = "";
+        }
+    });
 
 
 

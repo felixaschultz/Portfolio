9e3970716552bf076d11b2616304125b8646f27c
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-01 10:31:56 +0100

    Fix class attribute in LinkedIn component

--- src/components/Icons/LinkedIn/index.js
+++ src/components/Icons/LinkedIn/index.js
@@ -3,7 +3,7 @@ import "./Style.css";
 export default function LinkedIn({url}){
     return (
         <a title="LinkedIn" className="navigation__link" href={url} target="_blank" role="button">
-            <i class="icon linkedIn"></i>
+            <i className="icon linkedIn"></i>
         </a>
     )
 }

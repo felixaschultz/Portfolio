59413fd49bac2234e3c8fd8caac09444744d1b3b
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-03 22:16:55 +0100

    Update recommendations list

--- src/recommendations/Recommendations.js
+++ src/recommendations/Recommendations.js
@@ -11,5 +11,19 @@ export const recom = [
     {
         recommend: "Felix has many years of front-end experience and that can be seen. He will happily help you, if help is needed",
         author: "Rasmus Polaszk, Study Friend"
+    },
+    {
+        recommend: `Vi har brugt Felix og Intastellar Solutions, International i forbindelse med Soleng Festival, hvor vi havde brug for én, der både var kompetent med video/billede/kamera, og teknisk viden indenfor broadcast/livestream til festivalen.
+        Efter en kort snak fandt vi hurtigt en løsning, og vi er meget positivt overrasket over fleksibiliteten, kvaliteten, deres venlige og åbne tilgang samt prisen.
+        Overordnet set en fornøjelse at arbejde sammen med, og jeg kan varmt anbefale at samarbejde med firmaet og deres kompetente medarbejdere, specielt indenfor video/teknik/livestream.`,
+        author: "Rasmus Madsen, Formand - Soleng Festivalforening"
+    },
+    {
+        recommend: `Vi fik udarbejdet et logo til vores kommende startup-virksomhed. Resultatet er vi mere end tilfredse med. Hos Intastellar oplevede vi blandt andet god kommunikation, hurtig respons, brugbare inputs , samt exceptionel forståelse for det visuelle. Kan Klart anbefales.`,
+        author: "Casper C. Christensen, grundlægger - The-Bark"
+    },
+    {
+        recommend: `Really professional and fast work. I hired Felix, to maintain and improve my website and I am very happy with my decision as I´m a small business owner and can now focus more on my daily business.`,
+        author: "Marvin Fox"
     }
 ]

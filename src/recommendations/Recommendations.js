b66a427a027b7f94a097bc8a6468ae3e5fc84cad
Felix A. Schultz <felix.schultz@engapho.com>
2024-03-03 22:25:13 +0100

    Fixed a typo

--- src/recommendations/Recommendations.js
+++ src/recommendations/Recommendations.js
@@ -28,6 +28,6 @@ export const recom = [
     },
     {
         recommend: `Felix har arbejdet med at designe fire roll up’s på 0,85x2 meter til at præsentere Farmer’s Window (Digi-Eye) på Årets Business Event 2019 i Sønderborg, samt business cards til mig som grundlægger ink. logoet som ikon. Han har vist en ualmindelig fild og dedikation, særligt da deadlines for trykt nærmede sig og påkrævede meget sent nattearbejde.`,
-        author: "Philip Staib, Adm. dir. Digi-Eye ApS"
+        author: "Philip Staib, Adm. dir. - Digi-Eye ApS"
     }
 ]

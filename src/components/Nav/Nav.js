ba96911b9c5b154e4449a11ffde4e0ccf17bfd03
Felix A. Schultz <felix.schultz@engapho.com>
2024-09-02 13:19:28 +0200

    Update Nav.css and Nav.js to improve navigation styling and functionality

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -20,30 +20,32 @@ export default function Nav() {
 
     return (
         <>
-            <a className="logo" href="/"><img src={logo} /></a>
-            <nav className="navigation">
-                {
-                    local[location].navigation.links.map((link, key) => {
-                        return (
-                            (link.type === "dropdown") ? <section key={key} className="dropdown">
-                                <button className="dropdownButton" onClick={() => {
-                                    setShowDropdown(!showDropdown)
-                                }}>{link.name}</button>
-                                {(showDropdown) ? <section className="dropdownContent">
-                                    {
-                                        link.items.map((item, key) => {
-                                            return <a key={key} href={item.path} target="_blank">{item.name}</a>
-                                        })
-                                    }
-                                </section> : null}
-                            </section> : <NavLink key={key} style={({isActive})=>({
-                                borderBottom: isActive ? "#15b0ab solid 2px": '',
-                                opacity: isActive ? 1 : ""
-                            })} className={`navigation__link`} to={link.path}>{link.name}</NavLink>
-                        )
-                    })
-                }
-            </nav>
+            <section style={{ display: "flex", alignItems: "center" }}>
+                <a className="logo" href="/"><img src={logo} /></a>
+                <nav className="navigation">
+                    {
+                        local[location].navigation.links.map((link, key) => {
+                            return (
+                                (link.type === "dropdown") ? <section key={key} className="dropdown">
+                                    <button className="dropdownButton" onClick={() => {
+                                        setShowDropdown(!showDropdown)
+                                    }}>{link.name}</button>
+                                    {(showDropdown) ? <section className="dropdownContent">
+                                        {
+                                            link.items.map((item, key) => {
+                                                return <a key={key} href={item.path} target="_blank">{item.name}</a>
+                                            })
+                                        }
+                                    </section> : null}
+                                </section> : <NavLink key={key} style={({ isActive }) => ({
+                                    borderBottom: isActive ? "#15b0ab solid 2px" : '',
+                                    opacity: isActive ? 1 : ""
+                                })} className={`navigation__link`} to={link.path}>{link.name}</NavLink>
+                            )
+                        })
+                    }
+                </nav>
+            </section>
             <section className="flex iconContainer">
                 <section>
                     <button onClick={openLanguage} className="locationPicker">{location}</button>
@@ -52,7 +54,7 @@ export default function Nav() {
                             {
                                 local[location].navigation.langSelector.map((choosedLang, key) => {
                                     return <>
-                                        <button key={choosedLang} className="locationPicker" onClick={(e) => {setLocation(choosedLang.short), setShowLanguage(!showLanguage)}}>
+                                        <button key={choosedLang} className="locationPicker" onClick={(e) => { setLocation(choosedLang.short), setShowLanguage(!showLanguage) }}>
                                             {choosedLang.long}
                                         </button>
                                     </>
@@ -63,7 +65,7 @@ export default function Nav() {
                 </section>
                 <GitHub url="https://github.com/felixaschultz" />
                 <LinkedIn url="https://www.linkedin.com/in/felixaschultz" />
-                <iframe src="https://github.com/sponsors/felixaschultz/button" title="Sponsor felixaschultz" height="32" width="114" style={{border: 0, borderRadius: "6px"}}></iframe>
+                <iframe src="https://github.com/sponsors/felixaschultz/button" title="Sponsor felixaschultz" height="32" width="114" style={{ border: 0, borderRadius: "6px" }}></iframe>
             </section>
         </>
     )

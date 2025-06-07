63cb7616a7df226a84f8beaed6dcfba765a42266
Felix A. Schultz <felix.schultz@engapho.com>
2025-06-07 12:41:26 +0200

    Remove LICENSE file for main.bundle.js and update ReactDOM import in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import App from "./src/modules/App.js";
-const createRoot = window.ReactDOM.createRoot
+import React from "react";
+import ReactDOM from "react-dom/client";
 const container = document.getElementById('app');
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
+const root = ReactDOM.createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<App />);
